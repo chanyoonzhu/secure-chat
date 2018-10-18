8853db05e0c29d667d15e325c3d373a2a543a239
Your Name <zhuchanyoon@gmail.com>
2018-10-17 21:53:55 -0500

    add login page create user table use passport for user authentication

--- app.js
+++ app.js
@@ -1,19 +1,83 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // set the template engine
 app.set('view engine', 'ejs');
 
 // add middlewares
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('login');
 });
 
 // listening on port 3000
 server = app.listen(3000);
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.send("Welcome "+ req.query.username + "!!"));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findById(id, function(err, user) {
+    cb(err, user);
+  });
+});
+
+/* MONGOOSE SETUP */
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/myDatabase');
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+      username: String,
+      password: String
+    });
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+/* PASSPORT LOCAL AUTHENTICATION */
+
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        UserDetails.findOne({
+            username: username 
+        }, function(err, user) {
+            if (err) {
+            return done(err);
+            }
+
+            if (!user) {
+            return done(null, false);
+            }
+
+            if (user.password != password) {
+            return done(null, false);
+            }
+            return done(null, user);
+        });
+    }
+));
+
+app.post('/', 
+    passport.authenticate('local', { failureRedirect: '/error' }),
+    function(req, res) {
+        res.render('index');
+});
 
 //socket.io instantiation
 const io = require("socket.io")(server);
@@ -41,3 +105,14 @@ io.on('connection', (socket) => {
     	socket.broadcast.emit('typing', {username : socket.username});
     });
 });
+
+// cryto
+const crypto = require('crypto');
+
+function hashPassword(password, salt) {
+    var hash = crypto.createHash('sha256');
+    hash.update(password);
+    hash.update(salt);
+    return hash.digest('hex');
+}
+
