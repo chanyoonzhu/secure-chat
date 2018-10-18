48b93d60693af0388d8f6e103896eb811a2381d5
Your Name <zhuchanyoon@gmail.com>
2018-10-18 15:55:22 -0500

    validate users by unique usernames

--- app.js
+++ app.js
@@ -39,13 +39,17 @@ passport.deserializeUser(function(id, cb) {
 /* MONGOOSE SETUP */
 
 const mongoose = require('mongoose');
+const uniqueValidator = require("mongoose-unique-validator");
 mongoose.connect('mongodb://localhost/myDatabase');
 
 const Schema = mongoose.Schema;
 const UserDetail = new Schema({
-      username: String,
-      password: String
+      username: { type: String, required: true, unique: true },
+      password: { type: String, required: true }
     });
+
+UserDetail.plugin(uniqueValidator);
+
 const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
 
 /* PASSPORT LOCAL AUTHENTICATION */
