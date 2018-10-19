e0a05b99bd445c8e4043674756a9373a187301f2
Your Name <zhuchanyoon@gmail.com>
2018-10-18 21:11:04 -0500

    remove redundant code

--- app.js
+++ app.js
@@ -96,13 +96,3 @@ io.on('connection', (socket) => {
     });
 });
 
-// cryto
-const crypto = require('crypto');
-
-function hashPassword(password, salt) {
-    var hash = crypto.createHash('sha256');
-    hash.update(password);
-    hash.update(salt);
-    return hash.digest('hex');
-}
-
