9874cefcda1101691fcd46049556497d5195ad70
Bo Rao <bo@Bos-MacBook-Pro.local>
2018-11-30 17:59:10 -0600

    Add key-exchange

--- public/login.js
+++ public/login.js
@@ -1,10 +1,12 @@
 $(function(){
     var salt = $("#salt").val();
+    var dhPrime = $("#dhPrime").val();
     // save to localStorage
     $('#login-form').submit(function() {
         var $pw = $("#password");
         localStorage.setItem('pw', $pw.val());
         localStorage.setItem('salt', salt);
+        localStorage.setItem('dhPrime', dhPrime);
         return true;
     });
 });
